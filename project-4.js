87973a37390e61c1c998c428966b098e1b0cbd5e
Giselle <giselle.clas@gmail.com>
2022-07-19 19:31:11 +0200

    Project 4 dom manipulation (timer - stop watcher)

---
+++ project-4.js
@@ -0,0 +1,92 @@
+// variables for buttons
+
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+// variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// variables for leading zero
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours= 0;
+
+// variables for set interval and timerstatus
+let timerInterval = null;
+let timerStatus = "Stopped";
+
+
+// stop Watch function
+function stopWatch(){
+
+  seconds++
+
+  if(seconds / 60 === 1){
+    seconds = 0;
+    minutes ++;
+
+    if(minutes / 60 === 1){
+      minutes = 0;
+      hours++;
+    }
+  }
+
+  if(seconds < 10){
+    leadingSeconds = "0" + seconds.toString();
+  }
+  else{
+    leadingSeconds = seconds;
+  }
+
+  if(minutes < 10){
+    leadingMinutes = "0" + minutes.toString();
+  }
+  else{
+    leadingMinutes = minutes;
+  }
+
+  if(hours < 10){
+    leadingHours = "0" + hours.toString();
+  }
+  else{
+    LeadingHours = hours;
+  }
+
+
+  let displayTimer = document.getElementById('timer').innerText = leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
+}
+
+//to check and debug:
+//window.setInterval(stopWatch, 1000);
+
+
+/* ***** START AND STOP BUTTONS ***** */
+startStopBtn.addEventListener('click', function(){
+
+  if(timerStatus === "Stopped"){
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById('startStopBtn').innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+    timerStatus = "started";
+  }
+  else{
+    window.clearInterval(timerInterval);
+    document.getElementById('startStopBtn').innerHTML = `<i class="fa-solid fa-pause" id="play"></i>`;
+    timerStatus = "stopped";
+  }
+});
+
+
+/* ***** RESET BUTTON ***** */
+resetBtn.addEventListener('click', function(){
+
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.getElementById('timer').innerHTML = "00:00:00";
+});
